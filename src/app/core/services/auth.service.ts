060c8845140a6e38dae5fce29271047dd83c633b
xavierGIT04 <francoiswakegbandi@gmail.com>
2026-03-05 23:15:01 +0000

    Update import path for environment configuration

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, tap, throwError, catchError } from 'rxjs';
 import { AuthResponse } from '../models';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
